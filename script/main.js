e03e315805833d84dcd5f27fd22da4e622215651
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-02-18 12:05:54 +0100

    first js function

---
+++ script/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.onload = function () {
+    const searchBtn = document.getElementById('searchBtn');
+
+    searchBtn.onclick = function () {
+        document.getElementById('lyrics').innerText = "";
+    }
+}
