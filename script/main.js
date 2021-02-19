09d73d83985bfad19bd86c38a16981be4f6139e4
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-02-19 13:00:50 +0100

    get and print lyrics

--- script/main.js
+++ script/main.js
@@ -2,9 +2,50 @@
 
 window.onload = function () {
     const searchBtn = document.getElementById('searchBtn');
+    const key = '4294483c1fa9e154b8920e88d4e076a4';
 
     searchBtn.onclick = function () {
         document.getElementById('lyrics').innerText = "";
+        const input = document.getElementById('searchBar').value;
+        const searchWords = input.split(' ').join('%20');
+        getSong(searchWords, key);
     }
+}
+
+function getSong(searchWords, key) {
+    fetch('https://api.vagalume.com.br/search.excerpt?q=' + searchWords + '&limit=5&apikey=' + key)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.response.docs[0].id);
+            let songId = data.response.docs[0].id;
+            getLyrics(songId, key);
+        })
+        .catch(function (error) {
+            console.error(error.message);
+        });
+}
+
+function getLyrics(songId, key) {
+
+    fetch('https://api.vagalume.com.br/search.php?musid=' + songId + '&apikey=' + key)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data.mus[0].text);
+            document.getElementById('lyrics').innerText = data.mus[0].text;
+        })
+        .catch(function (error) {
+            console.error(error.message);
+        });
 }
 
