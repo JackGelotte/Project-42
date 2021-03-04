a852ae51d6282a7963734ffdb894a11c0764742b
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-03-04 15:06:31 +0100

    error handling song search

--- script/main.js
+++ script/main.js
@@ -6,7 +6,10 @@ window.onload = function () {
     const searchBtn = document.getElementById('searchBtn');
     const key = '4294483c1fa9e154b8920e88d4e076a4';
     searchBtn.onclick = function () {
-        document.getElementById('lyrics').innerText = "";
+        // gömmer lyrics-container när en ny sökning görs
+        document.querySelector('#lyrics-container').style.display = 'none';
+
+        // kalla på funktion som gör sökning med user input
         const input = document.getElementById('searchBar').value;
         const searchWords = input.split(' ').join('%20');
         getSongs(searchWords, key);
@@ -15,6 +18,9 @@ window.onload = function () {
 }
 
 function getSongs(searchWords, key) {
+    // se till att error-meddelande inte visas på default
+    document.getElementById('error-msg').style.visibility = 'hidden';
+
     fetch('https://api.vagalume.com.br/search.excerpt?q=' + searchWords + '&limit=5&apikey=' + key)
         .then(function (response) {
             if (response.ok) {
@@ -38,7 +44,9 @@ function getSongs(searchWords, key) {
             printSongResults(songArr, key, length);
         })
         .catch(function (error) {
-            console.error(error.message);
+            console.log(error.message);
+            document.getElementById('error-msg').style.visibility = 'visible';
+            document.getElementById('error-msg').innerHTML = 'Could not find any song that matches your search, try again!';
         });
 }
 
@@ -76,6 +84,8 @@ function printSongResults(songArr, key, length) {
 
 function embedVideo(band, song) {
     const youtubeKey = 'AIzaSyAMElWV5YhXPfB9z_DVSlgYTN_GM-CzXrk';
+    const video = document.getElementById('video');
+    const videoReplace = document.getElementById('video-replace');
 
     fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + band + song + '&type=video&videoEmbeddable=true&key=' + youtubeKey)
         .then(function (response) {
@@ -87,9 +97,13 @@ function embedVideo(band, song) {
         .then(function (data) {
             console.log(data);
             let videoId = data.items[0].id.videoId;
-            document.getElementById('video').data = 'https://www.youtube.com/embed/' + videoId;
+            video.style.display = 'block';
+            videoReplace.style.display = 'none';
+            video.data = 'https://www.youtube.com/embed/' + videoId;
         })
         .catch(function (error) {
+            video.style.display = 'none';
+            videoReplace.style.display = 'block';
             console.error(error.message);
         });
 }
@@ -105,7 +119,6 @@ function getLyrics(songId, key) {
         .then(function (data) {
             console.log(data);
             console.log(data.mus[0].text);
-            document.getElementById('title-links').style.display = 'hidden';
             document.getElementById('lyrics').innerText = data.mus[0].text;
             document.getElementById('search-title').innerText = data.mus[0].name;
         })
